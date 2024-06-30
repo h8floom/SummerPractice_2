c74563299d482915fc10225f5558584f28ec1060
h8floom <150369840+h8floom@users.noreply.github.com>
2024-06-30 21:29:14 +0400

    докинул 6ую практику
    докинул 6ую практику

---
+++ lol_kek/practice_6/Program.cs
@@ -0,0 +1,13 @@
+﻿double x = 0.7;
+int n = 10;
+double y = (Math.Exp(x) + Math.Exp(-x)) / 2;
+double s = 1;
+for (int i = 2; i <= n; i += 2) // исправление (в ряде тейлора степень у x начинается с 2, а не с 1)
+{
+    int k = 1; // факториал
+    for (int j = 2; j <= i; j++)
+        k *= j;
+    double slag = Math.Pow(x, i) / k;
+    s += slag;
+    Console.WriteLine("| {0}\t| {1}\t| {2}\t|", x, y, s);
+}
