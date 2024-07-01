9112f424568a747a499acfcad8449a9065e4a191
floom <nezoxwtf73@gmail.com>
2024-07-02 00:02:32 +0400

    допил

--- lol_kek/practice_6/Program.cs
+++ lol_kek/practice_6/Program.cs
@@ -1,13 +1,48 @@
-﻿double x = 0.7;
-int n = 10;
-double y = (Math.Exp(x) + Math.Exp(-x)) / 2;
-double s = 1;
-for (int i = 2; i <= n; i += 2) // исправление (в ряде тейлора степень у x начинается с 2, а не с 1)
+﻿using System;
+using System.IO;
+
+class Program
 {
-    int k = 1; // факториал
-    for (int j = 2; j <= i; j++)
-        k *= j;
-    double slag = Math.Pow(x, i) / k;
-    s += slag;
-    Console.WriteLine("| {0}\t| {1}\t| {2}\t|", x, y, s);
+    static void Main()
+    {
+        double x = 0.7;
+        int n = 10;
+        double y = (Math.Exp(x) + Math.Exp(-x)) / 2;
+        double s = 1;
+
+        string logFilePath = "log.txt";
+        using (StreamWriter writer = new StreamWriter(logFilePath))
+        {
+            DateTime startTime = DateTime.Now;
+            writer.WriteLine("====== Начало программы ======");
+            writer.WriteLine($"Время начала: {startTime}");
+            writer.WriteLine($"Значение x: {x}");
+            writer.WriteLine($"n: {n}");
+            writer.WriteLine($"Точное значение cosh(x): {y}");
+            writer.WriteLine($"Начальное значение s: {s}");
+            writer.WriteLine("| x\t| y\t| s\t| i\t| k\t| slag\t|");
+
+            for (int i = 2; i <= n; i += 2) // ряде Тейлора степень у x начинается с 2, а не с 1
+            {
+                int k = 1; // факториал
+                writer.WriteLine($"Вычисление факториала для i = {i}");
+                for (int j = 2; j <= i; j++)
+                {
+                    k *= j;
+                    writer.WriteLine($"j = {j}, k = {k}");
+                }
+                double slag = Math.Pow(x, i) / k;
+                s += slag;
+                string logEntry = $"| x = {x}\t| y = {y}\t| s = {s}\t| i = {i}\t| k = {k}\t| slag = {slag}\t|";
+
+                string res = $"| {x}\t| {y}\t| {s}\t| {i}\t| {k}\t| {slag}\t|";
+                writer.WriteLine(logEntry);
+                Console.WriteLine(res);
+            }
+
+            DateTime endTime = DateTime.Now;
+            writer.WriteLine("====== Конец программы ======");
+            writer.WriteLine($"Время конца: {endTime}");
+        }
+    }
 }
--- lol_kek/practice_6/obj/Debug/net8.0/practice_6.AssemblyInfo.cs
+++ lol_kek/practice_6/obj/Debug/net8.0/practice_6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practice_6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c74563299d482915fc10225f5558584f28ec1060")]
 [assembly: System.Reflection.AssemblyProductAttribute("practice_6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practice_6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
